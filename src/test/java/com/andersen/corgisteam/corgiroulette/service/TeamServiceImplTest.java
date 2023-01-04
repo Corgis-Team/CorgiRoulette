9c95bcd8279e55cba2eb59567542711cd430bfd0
MerakseS <the.sergei.losev@gmail.com>
2023-01-04 18:16:15 +0300

    #1 Add tests for team service and repository

---
+++ src/test/java/com/andersen/corgisteam/corgiroulette/service/TeamServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.andersen.corgisteam.corgiroulette.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.andersen.corgisteam.corgiroulette.entity.Team;
+import com.andersen.corgisteam.corgiroulette.repository.TeamRepository;
+import com.andersen.corgisteam.corgiroulette.service.exception.FieldLengthExceedException;
+import com.andersen.corgisteam.corgiroulette.service.exception.RequiredFieldIsEmptyException;
+
+class TeamServiceImplTest {
+
+    private TeamRepository teamRepository;
+    private TeamService teamService;
+
+    @BeforeEach
+    void setUp() {
+        teamRepository = Mockito.mock(TeamRepository.class);
+        teamService = new TeamServiceImpl(teamRepository);
+    }
+
+    @Test
+    void create() {
+        Team team = new Team("Blue");
+        Team createdTeam = new Team(1, "Blue");
+
+        Mockito.when(teamRepository.save(team)).thenReturn(createdTeam);
+        teamService.create(team);
+
+        Mockito.verify(teamRepository).save(team);
+    }
+
+    @Test
+    void createTeamWithEmptyName() {
+        Team team = new Team("");
+        Assertions.assertThrows(RequiredFieldIsEmptyException.class, () -> teamService.create(team));
+    }
+
+    @Test
+    void createTeamWithLargeName() {
+        Team team = new Team("jfaufhavkcjfiajflkdakvjioajffvmdffj");
+        Assertions.assertThrows(FieldLengthExceedException.class, () -> teamService.create(team));
+    }
+}

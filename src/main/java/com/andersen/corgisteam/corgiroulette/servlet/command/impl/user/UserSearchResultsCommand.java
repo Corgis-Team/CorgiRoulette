a2c9b70d018a17a6eddcb1e780f7c022eeec0e93
myo044 <zombiecsopistoletov@gmail.com>
2023-01-06 12:54:35 +0200

    In case of incorrect input into user search, user will be redirected to not found page

--- src/main/java/com/andersen/corgisteam/corgiroulette/servlet/command/impl/user/UserSearchResultsCommand.java
+++ src/main/java/com/andersen/corgisteam/corgiroulette/servlet/command/impl/user/UserSearchResultsCommand.java
@@ -20,7 +20,7 @@ public class UserSearchResultsCommand implements Command {
 
     private static final Logger log = LoggerFactory.getLogger(UserSearchResultsCommand.class);
     private static final String USER_SEARCH_RESULTS = "/WEB-INF/jsp/user/userSearchResults.jsp";
-    private static final String SEARCH_USER_PATH = "/WEB-INF/jsp/user/searchUser.jsp";
+    private static final String NOT_FOUND_PATH = "/WEB-INF/jsp/notFound.jsp";
     private static final String FULL_NAME_PARAMETER = "full_name";
     private static final String USERS_PARAMETER = "users";
     private static final String ERROR_ATTRIBUTE_NAME = "errorMessage";
@@ -42,7 +42,7 @@ public class UserSearchResultsCommand implements Command {
         catch (QueryExecutionException | ValidationException e) {
             log.warn("Can't receive users search results cause: ", e);
             request.setAttribute(ERROR_ATTRIBUTE_NAME, e.getMessage());
-            request.getRequestDispatcher(SEARCH_USER_PATH).forward(request, response);
+            request.getRequestDispatcher(NOT_FOUND_PATH).forward(request, response);
         }
     }
 }

c7f3af1de0fa50ab5527a2cc6d8d858d3a1d7d24
Polina Belkevich <pbelkevich@gmail.com>
2023-01-12 19:33:42 +0100

    Test for all pairs

--- src/test/java/com/andersen/corgisteam/corgiroulette/service/FindOpponentsUsingListTest.java
+++ src/test/java/com/andersen/corgisteam/corgiroulette/service/FindOpponentsUsingListTest.java
@@ -1,30 +1,51 @@
 package com.andersen.corgisteam.corgiroulette.service;
 
+import com.andersen.corgisteam.corgiroulette.database.DatabaseConfig;
+import com.andersen.corgisteam.corgiroulette.entity.Pair;
 import com.andersen.corgisteam.corgiroulette.entity.Team;
 import com.andersen.corgisteam.corgiroulette.entity.User;
-import com.andersen.corgisteam.corgiroulette.repository.PairRepository;
+import com.andersen.corgisteam.corgiroulette.repository.*;
 import com.andersen.corgisteam.corgiroulette.service.exception.NullListForGeneratePairException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 class FindOpponentsUsingListTest {
-
+    private static final Logger log = LoggerFactory.getLogger(FindOpponentsUsingListTest.class);
     private PairRepository pairRepository;
     private UserService userService;
 
     private FindOpponentsUsingList findOpponentsUsingList;
+    Connection connection;
+    UserRepository userRepository;
 
     @BeforeEach
     void setUp() {
         pairRepository = Mockito.mock(PairRepository.class);
         userService = Mockito.mock(UserService.class);
         findOpponentsUsingList = new FindOpponentsUsingList(userService, pairRepository);
+
+        try {
+            this.connection = DatabaseConfig.getConnection();
+            this.userRepository = new UserRepositoryImpl(new TeamRepositoryImpl());
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't get database connection");
+        }
     }
 
 
@@ -100,5 +121,22 @@ class FindOpponentsUsingListTest {
 
         List<User> usersTeammates = List.copyOf(users);
         Assertions.assertNotEquals(usersTeammates, findOpponentsUsingList.checkCountOfOpponents(originalUsersNotPicked.get(0), users, originalUsersNotPicked));
+    }
+
+    @Test
+    void createAllPairs() {
+        List <User> allUsers = userRepository.findAll();
+        System.out.println(allUsers);
+        for (int i = 0; i < (int)Math.ceil(allUsers.size()/2.0); i++) {
+            Pair pair = findOpponentsUsingList.createOpponents();
+            log.info("Create pair for user id {}", pair.getUser().getId());
+            log.info("Create pair for opponent id {}", pair.getOpponent().getId());
+            Assertions.assertEquals(pair, findOpponentsUsingList.createPairOfOpponents(pair.getUser(),pair.getUser()));
+        }
+    }
+
+    @AfterEach
+    public void after() throws SQLException {
+        this.connection.close();
     }
 }
